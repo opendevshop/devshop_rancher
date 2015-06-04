43b0a32b7bbec600310fad7ae45211a04b55c5ff
Jon Pugh <jon@thinkdrop.net>
2015-06-04 16:34:27 -0400

    Adding verify_server_cmd() override.

--- Provision/Service/db/rancher.php
+++ Provision/Service/db/rancher.php
@@ -6,7 +6,13 @@ class Provision_Service_db_rancher extends Provision_Service_db {
 
   function init_server() {
     parent::init_server();
+    drush_log('Provision_Service_db_rancher::init_server()', 'ok');
+  }
 
+  /**
+   * Verifies database connection and commands
+   */
+  function verify_server_cmd() {
     drush_log('Provision_Service_db_rancher::init_server()', 'ok');
   }
 }
