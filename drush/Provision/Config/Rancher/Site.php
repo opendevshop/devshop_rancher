e1758ed58c79247cb3a2c2c387c347fb3a9766b0
Jon Pugh <jon@thinkdrop.net>
2016-06-20 12:15:16 -0400

    Adding docker compose template from Terra, populating variables.

--- drush/Provision/Config/Rancher/Site.php
+++ drush/Provision/Config/Rancher/Site.php
@@ -9,10 +9,39 @@ class Provision_Config_Rancher_Site extends Provision_Config_Rancher {
   // The template file to use when the site has been disabled.
   public $description = 'docker compose file for this environment.';
 
+  function process() {
+    parent::process();
+
+    $project_name = d('@' . $this->uri)->project;
+    $environment_name = d('@' . $this->uri)->environment;
+
+    if ($environment = d('@project_' . $project_name)->getEnvironment($environment_name)) {
+
+      $this->data['environment'] = $environment;
+
+      // Prepare domains for VIRTUAL_HOST environment variable
+      $this->data['virtual_hosts'] = implode(',', $environment->domains);
+
+      // Doc Root
+      $this->data['document_root'] = d('@project_' . $project_name)->project['drupal_path'];
+
+      // MySQL Root password, generated.
+      $this->data['mysql_root_password'] = provision_password(32);
+
+      // @TODO: detect automatically.
+      $this->data['host_uid'] = '12345';
+      $this->data['host_gid'] = '12345';
+
+      $this->data['authorized_keys'] = file_get_contents('/var/aegir/.ssh/id_rsa.pub');
+
+      drush_log($project_name . print_r($environment, 1), 'devshop_ok');
+    }
+    else {
+      return;
+    }
+  }
 
   function filename() {
-    drush_log('==========================================', 'ok');
-    drush_log('Provision_Config_Rancher_Site::filename()', 'ok');
     return $this->data['server']->http_app_path . '/' . $this->uri . '/docker-compose.yml';
   }
 }
--- drush/Provision/Config/Rancher/docker-compose.yml.php
+++ drush/Provision/Config/Rancher/docker-compose.yml.php
@@ -1,2 +1,51 @@
-db:
-  image: mariadb
+load:
+    image: tutum/haproxy
+    environment:
+        VIRTUAL_HOST: <?php print $virtual_hosts . "\n"; ?>
+
+    links:
+        - app
+    expose:
+        - 80/tcp
+    ports:
+        - '80'
+    restart: on-failure
+app:
+    image: terra/drupal
+    tty: true
+    stdin_open: true
+    links:
+        - database
+    volumes:
+        - '<?php print $environment->repo_root; ?>:/app'
+    environment:
+        HOST_UID: <?php print $host_uid . "\n" ?>
+        HOST_GID: <?php print $host_gid . "\n" ?>
+        DOCUMENT_ROOT: <?php print $document_root . "\n" ?>
+    expose:
+        - 80/tcp
+    restart: on-failure
+database:
+    image: mariadb
+    tty: true
+    stdin_open: true
+    environment:
+        MYSQL_ROOT_PASSWORD: <?php print $mysql_root_password . "\n" ?>
+        MYSQL_DATABASE: <?php print urlencode($db_name) . "\n"; ?>
+        MYSQL_USER: <?php print urlencode($db_user) . "\n"; ?>
+        MYSQL_PASSWORD:  <?php print urlencode($db_passwd) . "\n"; ?>
+    restart: on-failure
+drush:
+    image: terra/drush
+    tty: true
+    stdin_open: true
+    links:
+        - database
+    ports:
+        - '22'
+    volumes:
+        - '<?php print $environment->root; ?>:/var/www/html'
+        - '<?php print $environment->repo_root; ?>:/source'
+    environment:
+        AUTHORIZED_KEYS: "<?php print $authorized_keys ?>"
+    restart: on-failure
